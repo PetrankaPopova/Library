5beca6b988fe3a8e54c484e5e9d9bb489ea26ee5
Jordan Georgiev <jordangeorgiev89@gmail.com>
2020-09-16 14:13:32 +0300

    BookStore - serviceModel, bindingModel, view

---
+++ src/main/java/library/model/service/BookStoreServiceModel.java
@@ -0,0 +1,125 @@
+package library.model.service;
+
+public class BookStoreServiceModel {
+
+    private String storeName;
+    private String storePhoneNumber;
+    private String storeHoursOfOperation;
+    private String country;
+    private String region;
+    private String postCode;
+    private String city;
+    private String streetAddress;
+    private String streetNumber;
+    private String vatNumber;
+    private String information;
+    private String termsAndConditions;
+    private String faq;
+
+    public BookStoreServiceModel() {
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getStorePhoneNumber() {
+        return storePhoneNumber;
+    }
+
+    public void setStorePhoneNumber(String storePhoneNumber) {
+        this.storePhoneNumber = storePhoneNumber;
+    }
+
+    public String getStoreHoursOfOperation() {
+        return storeHoursOfOperation;
+    }
+
+    public void setStoreHoursOfOperation(String storeHoursOfOperation) {
+        this.storeHoursOfOperation = storeHoursOfOperation;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getVatNumber() {
+        return vatNumber;
+    }
+
+    public void setVatNumber(String vatNumber) {
+        this.vatNumber = vatNumber;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    public String getTermsAndConditions() {
+        return termsAndConditions;
+    }
+
+    public void setTermsAndConditions(String termsAndConditions) {
+        this.termsAndConditions = termsAndConditions;
+    }
+
+    public String getFaq() {
+        return faq;
+    }
+
+    public void setFaq(String faq) {
+        this.faq = faq;
+    }
+}
