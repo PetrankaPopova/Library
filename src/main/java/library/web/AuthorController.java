569ba335015d387bd01e87dd42b1434f073a092c
mirozspace <zstefchev@gmail.com>
2020-09-16 17:26:48 +0200

    .

--- src/main/java/library/web/AuthorController.java
+++ src/main/java/library/web/AuthorController.java
@@ -6,6 +6,7 @@ import library.service.AuthorService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthorController {
 
     private final AuthorService authorService;
