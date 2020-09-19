f5630af95062bb5d00c4839727098f832108e4e2
StanislavIliev <slavi19vd@gmail.com>
2020-09-19 23:50:47 +0100

    Banner

---
+++ src/main/java/library/validation/BannerAddValidator.java
@@ -0,0 +1,47 @@
+package library.validation;
+
+import library.model.bindingmodel.BannerAddBindingModel;
+import library.repository.BannerRepository;
+import org.springframework.validation.Errors;
+
+@Validator
+public class BannerAddValidator implements org.springframework.validation.Validator {
+
+   private final BannerRepository bannerRepository;
+
+    public BannerAddValidator(BannerRepository bannerRepository) {
+        this.bannerRepository = bannerRepository;
+    }
+
+        @Override
+        public void validate(Object o, Errors errors) {
+            BannerAddBindingModel bannerAddBindingModel = (BannerAddBindingModel) o;
+
+            if (this.bannerRepository.findByCompanyName(bannerAddBindingModel.getCompanyName()).isPresent()) {
+                errors.rejectValue("companyName",
+                        String.format(ValidationConstants.COMPANY_ALREADY_EXISTS, bannerAddBindingModel.getCompanyName()),
+                        String.format(ValidationConstants.COMPANY_ALREADY_EXISTS, bannerAddBindingModel.getCompanyName())
+                );
+            }
+
+            if (bannerAddBindingModel.getPrice() <=   0) {
+                errors.rejectValue("price",
+                        ValidationConstants.PRICE_NEGATIVE,
+                        ValidationConstants.PRICE_NEGATIVE
+                );
+            }
+
+
+        }
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return false;
+    }
+
+}
+
+
+
+
+
+
