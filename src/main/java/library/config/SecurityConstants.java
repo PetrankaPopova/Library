5a7b15189f8527598d8e58829629b27ed6710ce3
mirozspace <zstefchev@gmail.com>
2020-10-11 10:03:38 +0200

    .

--- src/main/java/library/config/SecurityConstants.java
+++ src/main/java/library/config/SecurityConstants.java
@@ -2,9 +2,9 @@ package library.config;
 
 public class SecurityConstants {
 
-    public static final String SECRET = "^[a-zA-Z0-9._]+$\r\nGuidelines89797987forAlphabeticalArraNumeralsandOtherSymbo$";
-    public static final long EXPIRATION_TIME = 432_000_000; // 5 days
-    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String SECRET = "^[a-zA-Z0-9._]+$\r\nGuidelines89797987forAlphabeticalArrayNumbersAndOtherSymbols$";
+    public static final long EXPIRATION_TIME = 400_000_000; // 5 days
+    public static final String TOKEN_PREFIX = "BookStore ";
     public static final String HEADER_TYPE = "Authorization";
     public static final String CLIENT_DOMAIN_URL = "*";
 
