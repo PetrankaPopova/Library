c8c75fc22cd999a5e6d25869c5212ad2a9f1e011
mirozspace <zstefchev@gmail.com>
2020-10-11 10:18:07 +0200

    .

--- src/main/java/library/model/service/UserServiceModel.java
+++ src/main/java/library/model/service/UserServiceModel.java
@@ -3,10 +3,17 @@ package library.model.service;
 import library.model.entity.Address;
 import library.model.entity.Cart;
 import library.model.entity.Role;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.List;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserServiceModel extends BaseServiceModel {
 
     private String username;
@@ -17,71 +24,13 @@ public class UserServiceModel extends BaseServiceModel {
     private List<AddressServiceModel> address;
     private CartServiceModel cartServiceModel;
     private Set<RoleServiceModel> roles;
+    private String country;
+    private String street;
+    private String city;
+    private String state;
+    private String postCode;
+    private String region;
+    private String streetNumber;
 
-    public UserServiceModel() {
-    }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public CartServiceModel getCartServiceModel() {
-        return cartServiceModel;
-    }
-
-    public void setCartServiceModel(CartServiceModel cartServiceModel) {
-        this.cartServiceModel = cartServiceModel;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public List<AddressServiceModel> getAddress() {
-        return address;
-    }
-
-    public void setAddress(List<AddressServiceModel> address) {
-        this.address = address;
-    }
-
-    public Set<RoleServiceModel> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<RoleServiceModel> roles) {
-        this.roles = roles;
-    }
 }
