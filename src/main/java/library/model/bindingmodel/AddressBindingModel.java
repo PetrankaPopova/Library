9c57b4a07549053b06990edf842e43ed70f8439f
mirozspace <zstefchev@gmail.com>
2020-12-23 17:49:19 +0100

    .

--- src/main/java/library/model/bindingmodel/AddressBindingModel.java
+++ src/main/java/library/model/bindingmodel/AddressBindingModel.java
@@ -1,82 +1,30 @@
 package library.model.bindingmodel;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.DecimalMin;
 
+@Setter
+@Getter
+@NoArgsConstructor
 public class AddressBindingModel {
 
+    @Length(min = 3, max = 20, message = "Country name should be between 3 and 20 characters.")
     private String country;
+    @Length(min = 3, max = 20, message = "Street name should be between 3 and 20 characters.")
     private String street;
+    @Length(min = 3, max = 20, message = "City name should be between 3 and 20 characters.")
     private String city;
+    @Length(min = 3, max = 20, message = "State name should be between 3 and 20 characters.")
     private String state;
+    @Length(min = 3, max = 20, message = "Post code should be between 3 and 20 characters.")
     private String postCode;
+    @Length(min = 3, max = 20, message = "Region name should be between 3 and 20 characters.")
     private String region;
+    @DecimalMin(value = "0",message = "Enter a valid street number.")
     private String streetNumber;
 
-    public AddressBindingModel() {
-    }
-
-    @Length(min = 3, max = 20, message = "Country name should be between 3 and 20 characters.")
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    @Length(min = 3, max = 20, message = "Street name should be between 3 and 20 characters.")
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    @Length(min = 3, max = 20, message = "City name should be between 3 and 20 characters.")
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    @Length(min = 3, max = 20, message = "State name should be between 3 and 20 characters.")
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    @Length(min = 3, max = 20, message = "Post code should be between 3 and 20 characters.")
-    public String getPostCode() {
-        return postCode;
-    }
-
-    public void setPostCode(String postCode) {
-        this.postCode = postCode;
-    }
-
-    @Length(min = 3, max = 20, message = "Region name should be between 3 and 20 characters.")
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    @DecimalMin(value = "0",message = "Enter a valid street number.")
-    public String getStreetNumber() {
-        return streetNumber;
-    }
-
-    public void setStreetNumber(String streetNumber) {
-        this.streetNumber = streetNumber;
-    }
 }
--- src/main/java/library/model/bindingmodel/BookBindingModel.java
+++ src/main/java/library/model/bindingmodel/BookBindingModel.java
@@ -1,195 +1,58 @@
 package library.model.bindingmodel;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 
+@Setter
+@Getter
+@NoArgsConstructor
 public class BookBindingModel {
 
+    @Pattern(regexp = "[a-zA-Z]", message = "Title is not correct!")
     private String title;
+    @Pattern(regexp = "[A-Z0-9]+", message = "ISBN number is not correct!")
     private String isbn;
-    private String[] categories;
+    @Pattern(regexp = "[0-9]+", message = "Year is not correct!")
     private String yearOfIssue;
+    @Pattern(regexp = "[a-zA-Z]+", message = "Publishing house is not correct")
     private String publishingHouse;
+    @Pattern(regexp = "[a-zA-Z]+", message = "Availability status is not correct!")
     private String availabilityStatus;
+    @Pattern(regexp = "[0-9]+", message = "Number of pages is not correct!")
     private int numberOfPages;
+    @Pattern(regexp = "[a-zA-Z]+", message = "Cover Type is not correct!")
     private String coverType;
+    @Pattern(regexp = "[a-zA-Z.;0-9-]+", message = "Content is not correct!")
     private String content;
+    @Pattern(regexp = "[a-zA-Z.;0-9-]+", message = "Description is not correct!")
     private String description;
+    @Pattern(regexp = "[a-zA-Z]+", message = "Language is not correct!")
     private String language;
+    @Pattern(regexp = "[0-9]+", message = "Rating is not correct!")
     private double rating;
+    @DecimalMin(value = "0.01", message = "Incorrect price!")
+    @DecimalMax(value = "150.00", message = "Incorrect price!")
     private BigDecimal price;
+    @DecimalMin(value = "1", message = "Incorrect quantity!")
+    @DecimalMax(value = "100", message = "Incorrect quantity!")
     private int quantity;
+    @DecimalMin(value = "1", message = "Incorrect quantity!")
+    @DecimalMax(value = "100", message = "Incorrect quantity!")
     private BigDecimal discount;
 
     private AuthorBindingModel author;
     private SizeBindingModel size;
-
+    private String[] categories;
     private MultipartFile[] photos;
 
-    public BookBindingModel() {
-    }
 
-    public AuthorBindingModel getAuthor() {
-        return author;
-    }
 
-    public void setAuthor(AuthorBindingModel author) {
-        this.author = author;
-    }
 
-    public SizeBindingModel getSize() {
-        return size;
-    }
-
-    public void setSize(SizeBindingModel size) {
-        this.size = size;
-    }
-
-    //@Pattern(regexp = "[a-zA-Z]", message = "Title is not correct!")
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    //@Pattern(regexp = "[A-Z0-9]+", message = "ISBN number is not correct!")
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    //???
-    public String[] getCategories() {
-        return categories;
-    }
-
-    public void setCategories(String[] categories) {
-        this.categories = categories;
-    }
-
-    //@Pattern(regexp = "[0-9]+", message = "Year is not correct!")
-    public String getYearOfIssue() {
-        return yearOfIssue;
-    }
-
-    public void setYearOfIssue(String yearOfIssue) {
-        this.yearOfIssue = yearOfIssue;
-    }
-
-    //@Pattern(regexp = "[a-zA-Z]+", message = "Publishing house is not correct")
-    public String getPublishingHouse() {
-        return publishingHouse;
-    }
-
-    public void setPublishingHouse(String publishingHouse) {
-        this.publishingHouse = publishingHouse;
-    }
-
-    //@Pattern(regexp = "[a-zA-Z]+", message = "Availability status is not correct!")
-    public String getAvailabilityStatus() {
-        return availabilityStatus;
-    }
-
-    public void setAvailabilityStatus(String availabilityStatus) {
-        this.availabilityStatus = availabilityStatus;
-    }
-
-    //@Pattern(regexp = "[0-9]+", message = "Number of pages is not correct!")
-    public int getNumberOfPages() {
-        return numberOfPages;
-    }
-
-    public void setNumberOfPages(int numberOfPages) {
-        this.numberOfPages = numberOfPages;
-    }
-
-    //@Pattern(regexp = "[a-zA-Z]+", message = "Cover Type is not correct!")
-    public String getCoverType() {
-        return coverType;
-    }
-
-    public void setCoverType(String coverType) {
-        this.coverType = coverType;
-    }
-
-    //@Pattern(regexp = "[a-zA-Z.;0-9-]+", message = "Content is not correct!")
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    //@Pattern(regexp = "[a-zA-Z.;0-9-]+", message = "Content is not correct!")
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    //@Pattern(regexp = "[a-zA-Z]+", message = "Language is not correct!")
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    //@Pattern(regexp = "[0-9]+", message = "Rating is not correct!")
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
-
-    //@DecimalMin(value = "0.01", message = "Incorrect price!")
-    //@DecimalMax(value = "150.00", message = "Incorrect price!")
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    //@DecimalMin(value = "1", message = "Incorrect quantity!")
-    //@DecimalMax(value = "100", message = "Incorrect quantity!")
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    //@DecimalMin(value = "1", message = "Incorrect discount!")
-    //@DecimalMax(value = "100", message = "Incorrect discount!")
-    public BigDecimal getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(BigDecimal discount) {
-        this.discount = discount;
-    }
-
-    //???
-    public MultipartFile[] getPhotos() {
-        return photos;
-    }
-
-    public void setPhotos(MultipartFile[] photos) {
-        this.photos = photos;
-    }
 }
